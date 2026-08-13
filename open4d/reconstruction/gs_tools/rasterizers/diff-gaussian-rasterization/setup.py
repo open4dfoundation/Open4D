6d5faf3906fe133751e37b2b605d2ee1762fe2bf
Ryan Kim <mwkjello@gmail.com>
2026-08-13 17:06:00 -0400

    Move the three rasterizers into gs_tools/rasterizers

--- open4d/reconstruction/gs_tools/rasterizers/diff-gaussian-rasterization/setup.py
+++ open4d/reconstruction/gs_tools/rasterizers/diff-gaussian-rasterization/setup.py
@@ -15,19 +15,18 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    version="1.0.0+3dgstream",
-    name="gstream_rasterization",
-    packages=['gstream_rasterization'],
+    name="diff_gaussian_rasterization",
+    packages=['diff_gaussian_rasterization'],
     ext_modules=[
         CUDAExtension(
-            name="gstream_rasterization._C",
+            name="diff_gaussian_rasterization._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../gs_tools/glm/")]})
+            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../glm/")]})
         ],
     cmdclass={
         'build_ext': BuildExtension
