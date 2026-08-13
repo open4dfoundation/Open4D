4f2f567bd8439e71a5ac5a0b500df1289d510717
Ryan Kim <mwkjello@gmail.com>
2026-08-13 16:14:36 -0400

    Motified setup, moved shared repos to tools, including a single enrionment

--- open4d/reconstruction/3dgstream/submodules/diff-gaussian-rasterization/setup.py
+++ open4d/reconstruction/3dgstream/submodules/diff-gaussian-rasterization/setup.py
@@ -16,18 +16,18 @@ os.path.dirname(os.path.abspath(__file__))
 
 setup(
     version="1.0.0+3dgstream",
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="gstream_rasterization",
+    packages=['gstream_rasterization'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="gstream_rasterization._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../gs_tools/glm/")]})
         ],
     cmdclass={
         'build_ext': BuildExtension
